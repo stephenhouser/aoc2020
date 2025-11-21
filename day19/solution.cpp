c1b2a633df6918c7bba2893d7fa653043745f480
Stephen Houser <stephenhouser@gmail.com>
2025-11-20 20:16:44 -0500

    day 19 part 2 done.

--- day19/solution.cpp
+++ day19/solution.cpp
@@ -13,28 +13,33 @@
 #include <unordered_map>
 #include <cctype>
 
-#include "mrf.h"	// map, reduce, filter templates
 #include "split.h"	// split strings
 
 using namespace std;
 
 struct rule_t {
 	size_t id = 0;
-	char ch = ' ';
-	vector<size_t> left = {};
-	vector<size_t> right = {};
+	char ch = '\0';
+	vector<vector<size_t>> rules = {};
 
 	rule_t(const string& rule) {
 		auto parts = split(rule, ":|\"");
 		id = stoul(parts[0]);
 		if (isdigit(parts[1][1])) {
-			left = split_size_t(parts[1]);
+			rules.push_back(split_size_t(parts[1]));
 			if (parts.size() > 2) {
-				right = split_size_t(parts[2]);
+				rules.push_back(split_size_t(parts[2]));
 			}
 		} else {
 			ch = parts[2][0];
 		}
+	}
+
+	rule_t(size_t id, const vector<vector<size_t>>& rules) :
+		id(id), rules(rules) {
+	}
+
+	rule_t() : id(0), rules({}) {
 	}
 };
 
@@ -73,43 +78,40 @@ const data_t read_data(const string& filename) {
 	return {rules, messages};
 }
 
-// trys to match all rules in vector
-// return 0 if failed, or number of tokens consumed from message
-size_t match_rule(const rules_t& rules, const size_t rule_n, const string& message);
+/* match -- attempt to match message to a sequence of rules from the ruleset (rules)
+ * message = abbbba
+ * sequence = {0, 5, 6}	the rules we need to match for this to be a valid message
+ * 
+ * returns true if the seqence can match the message, false otherwise
+*/
+bool match(const rules_t& rules, const string& message, const vector<size_t>& sequence) {
+	if (message.empty() || sequence.size() == 0) {
+		// if both are at end we have matched!
+		return message.empty() && sequence.size() == 0;
+	}
 
-size_t match_rules(const rules_t& rules, const vector<size_t> match_r, const string& message) {
-	size_t consumed = 0;
-	string msg = message;
+	const rule_t& rule = rules.at(sequence[0]);
+	const vector<size_t> remainder(sequence.begin()+1, sequence.end());
 
-	// all the rules in the rule set given must match
-	for (const auto rule_n : match_r) {
-		size_t local = match_rule(rules, rule_n, msg);
-		if (local == 0) {
-			return 0;
+	if (rule.ch) {
+		if (rule.ch == message[0]) {
+			return match(rules, message.substr(1), remainder);
+		} 
+
+		return false;
+	}
+
+	for (const auto& child_rule : rule.rules) {
+		// prepend the child rules to the remaining sequence
+		vector<size_t> child_sequence(child_rule.begin(), child_rule.end());
+		child_sequence.insert(child_sequence.end(), remainder.begin(), remainder.end());
+
+		if (match(rules, message, child_sequence)) {
+			return true;
 		}
-
-		consumed += local;
-		msg = msg.substr(local);
 	}
 
-	return consumed;
-}
-
-size_t match_rule(const rules_t& rules, const size_t rule_n, const string& message) {
-	rule_t rule = rules.at(rule_n);
-
-	// rule is a leaf node, it it matches it consumes a token, if not, 0
-	if (rule.ch != ' ') {
-		return rule.ch == message[0] ? 1 : 0;
-	}
-
-	// rule is not a leaf node, see if the left or right match
-	size_t matches = match_rules(rules, rule.left, message);
-	if (matches == 0) {
-		matches = match_rules(rules, rule.right, message);
-	}
-
-	return matches;
+	return false;
 }
 
 /* Part 1 */
@@ -119,17 +121,34 @@ result_t part1(const data_t& data) {
 
 	result_t matches = 0;
 	for (const auto& message : messages) {
-		result_t local = match_rule(rules, 0, message);
-
-		// print("{} -> {}\n", message, local);
-		matches += (local == message.size()) ? 1 : 0ul;
+		auto matched = match(rules, message, {0});
+		// print("{} -> {}\n", message, matched);
+		matches += matched ? 1 : 0ul;
 	}
 
 	return matches;
 }
 
-result_t part2([[maybe_unused]] const data_t& data) {
-	return 0;
+// 176 too low
+// 265 too high
+// 260 just right
+result_t part2(const data_t& data) {
+	rules_t rules = data.first;
+	const messages_t& messages = data.second;
+
+	// replace these two rules, as per the instructions
+	// they create loops!
+	rules[8] = {8, {{42}, {42, 8}}};
+	rules[11] = {11, {{42, 31}, {42, 11, 31}}};
+
+	result_t matches = 0;
+	for (const auto& message : messages) {
+		auto matched = match(rules, message, {0});
+		// print("{} -> {}\n", message, matched);
+		matches += matched ? 1 : 0ul;
+	}
+
+	return matches;
 }
 
 int main(int argc, char* argv[]) {
